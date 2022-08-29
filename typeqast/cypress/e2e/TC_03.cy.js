3c8878d67ba4f1bc0db5851c1a90311ab27ac2e9
Srdjan Tanasijevic <srdjan.tanasijevic@stech.rs>
2022-08-29 09:35:13 +0200

    typeqast

--- typeqast/cypress/e2e/TC_03.cy.js
+++ typeqast/cypress/e2e/TC_03.cy.js
@@ -7,19 +7,20 @@ describe('TC-03', () => {
   it('Registration', () => {
     cy.visit("https://www.links.hr/hr/login")
     cy.get("[class='ok-button button-1']").click()
-    //linksLoginP.TitleCustomer().contains("Postojeći korisnik")
-    //linksLoginP.Email().type("uniqatest22@gmail.com")
-    //linksLoginP.Password().type("test22")
+    linksLoginP.TitleCustomer().contains("Postojeći korisnik")
+    linksLoginP.Email().type("uniqatest22@gmail.com")
+    linksLoginP.Password().type("test22")
     //cy.wait(2000)
     //cy.get('iframe[title="reCAPTCHA"]')
     //.first()
     //.then((recaptchaIframe) => {
     //  const body = recaptchaIframe.contents()
     //  cy.wrap(body).find('.recaptcha-checkbox-border').should('be.visible').click()
-    //  })
-    //linksLoginP.Recaptcha().click()
-    ///
-    //linksLoginP.Submit().eq(1).click()
+    //   })
+    
+    linksLoginP.Recaptcha().click()
+    
+    linksLoginP.Submit().eq(1).click()
   })
 
   
--- typeqast/cypress/mapping/linksLoginP.js
+++ typeqast/cypress/mapping/linksLoginP.js
@@ -9,8 +9,11 @@ class linksLogin {
         return cy.get("[id='Password']")
     }
     Recaptcha() {
-        return cy.get("#rc-anchor-container").parent().find("[class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']")
+        return cy.get("[class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']")
     }
+    //Recaptcha() {
+    //    return cy.get("[id='recaptcha - anchor']")
+    //}
     Submit() {
         return cy.get("[type='submit']")
     }
